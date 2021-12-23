7a6b8c6a3e475cde8ed9f0de8922706ff2004143
Baptiste <baptistebareau@gmail.com>
2021-12-23 01:30:56 +0100

    Update consultation.java

--- CentreMedical/src/consultation.java
+++ CentreMedical/src/consultation.java
@@ -1,1 +1,22 @@
+public class consultation{
+   String nom;
+   String nommedecin;
+   String dateconsultation;
+   String pathologie ;
+   String appareil;
+   String octroyé;  
+ // squelette des différentes fonctions que l'ont va avoir besoins pour la classe consultation
+   public void chercherconsultation(){
 
+   }
+   public void supprimerconsultation(){
+
+   }
+   public void creeruneconsultation(){
+
+   }
+   public void modfifierconsultation(){
+
+   }
+
+} 
