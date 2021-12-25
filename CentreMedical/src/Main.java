30228c4a374f2b7aaf2d2ff9fd25c67102276543
sir-martino <martin.neron@laposte.net>
2021-12-25 16:21:03 +0100

    ModifierPatient() qui marche tout bien

--- CentreMedical/src/Main.java
+++ CentreMedical/src/Main.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class Main {
    
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+
        
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("nom patient");
         String nom = sc.nextLine();
        
@@ -18,15 +20,14 @@ public class Main {
         System.out.println("numero securite sociale patient 13 chiffres max");
 
         String  NbSSocial = sc.nextLine();
-        verif(NbSSocial);
-
+        verifier(NbSSocial);
 
         System.out.println("date  de naissance patient");
         String dateNaissance = sc.nextLine();
         
         patient pat = new patient(nom, prenom, NbSSocial, dateNaissance);
         
-
+/*
         System.out.println("nom patient consultation");
         String nompat = sc.nextLine();
 
@@ -48,39 +49,42 @@ public class Main {
         
         medecin consul = new medecin (nompat , nommedecin, dateconsultation,pathologie,  appareil, octroye);
         consul.ecrireconsultation();
+ */
+       
         try{
             pat.ecrirePatient();
-            pat.rechercherPatient("martin");
+          //  pat.rechercherPatient("toto");
+            pat.modifierpatient("dinosaure");
         }catch( PatientInexistantException e){
             System.out.println("ce patient n'existe pas");
         }
         
     }
+    public static void verifier(String NbSSocial) throws IOException{
 
-    public static void verif(String NbSSocial) throws IOException{
-
-     //  = numero seciurite sociale 
-     // verification nombre si c'est des chiffres //si il y en a 13 pile
-
-     char Chiffre =' ';
-     if(NbSSocial.length()<13 || NbSSocial.length()>13){
-         throw new NbSecuriteSocialeException("pas le bon nombre de caracteres saisis ");
-     }
-     // initialisation du tableau de l'alphabet
-     char Alphabet[] = new char [26];
-     for(int i=0; i<NbSSocial.length();i++){
-        Chiffre = NbSSocial.charAt(i);
-         // utilisation ascii
-        for(int j =97;j<122;j++){
-            if(Chiffre ==(Alphabet[i])){
-                throw new IOException(" erreur de  saisi");
-                
+        //  = numero seciurite sociale 
+        // verification nombre si c'est des chiffres //si il y en a 13 pile
+   
+        char Chiffre =' ';
+        if(NbSSocial.length()<13 || NbSSocial.length()>13){
+            throw new NbSecuriteSocialeException("pas le bon nombre de caracteres saisis ");
         }
-
-     }
-     
-     }
-    }
+        // initialisation du tableau de l'alphabet
+        char Alphabet[] = new char [26];
+        for(int i=0; i<NbSSocial.length();i++){
+           Chiffre = NbSSocial.charAt(i);
+            // utilisation ascii
+           for(int j =97;j<122;j++){
+               if(Chiffre ==(Alphabet[i])){
+                   throw new IOException(" erreur de  saisi");
+                   
+           }
+   
+        }
+        
+        }
+       }
+   
 }
 
 
--- CentreMedical/src/patient.java
+++ CentreMedical/src/patient.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 //import java.util.Scanner;
 import java.io.Reader;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import javax.annotation.processing.Filer;
@@ -95,10 +96,9 @@ public class patient {
                     String str = lectureFichier.nextLine();
                     String[] mots = str.split("/");
                     // vérifie le nom qui est a la 2eme palce du tableau
-                    if(mots[1].equalsIgnoreCase(nomPatient)){
+                    if(mots[1].equalsIgnoreCase(nomPatient.trim())){
 
                         System.out.println("fichier du patient "+nomPatient+"  trouvé" );
-                        System.out.println( getNom() + "/" + getPrenom() + "/" + getNbSScoial() + "/" +  getDateNaissance() +"\n"); 
                         break ;
 
 
@@ -113,13 +113,17 @@ public class patient {
 
 
         public void supprimerPatient(String nomPatient)throws IOException{
+            // lecture du fichier deja existant
+            //creation dans un nouveau fichier
             File Fichier = new File("ListePatient.txt");
             File ModifFichier = new File("ListePatientModifie.txt");
             FileReader FileReader = new FileReader(Fichier);
             BufferedReader lecture = new BufferedReader(FileReader);
             FileWriter fileWriter = new FileWriter(ModifFichier);
             String Ligne = null;
-           
+        //recherche de la ligne
+        //si c'est pas la bonne on reecris la meme ligne dans le nouveau fichier et on saute une ligne.
+        //si c'est la bonne on ecris rien et on ne sate pas de ligne
             while((Ligne = lecture.readLine()) != null){
                 String[] mots = Ligne.split("/");
                 if(mots[1].equalsIgnoreCase(nomPatient)){
@@ -131,11 +135,12 @@ public class patient {
 
                  fileWriter.flush();
             }
+            //fermeture
             lecture.close();
             fileWriter.close();
             Fichier.delete();
             
-           
+            // verification que le nouveau fichier a ete renommé 
             if(ModifFichier.renameTo(Fichier)){
                 System.out.println("Le fichier a été renommé avec succès");
               }else{
@@ -144,27 +149,147 @@ public class patient {
             }
 
 
-        public void modifierpatient(String motard)throws FileNotFoundException{
-            FileReader lecteur = new FileReader("ListePatient.txt");
-            try (Scanner lectureFichier = new Scanner(lecteur)) {
-                while(lectureFichier.hasNextLine()){
-                    String str = lectureFichier.nextLine();
-                    String[] mots = str.split("/");
-                    // vérifie le nom qui est a la 2eme palce du tableau
-                    if(mots[1].equalsIgnoreCase(motard)&&mots[2].equalsIgnoreCase(motard)&&mots[3].equalsIgnoreCase(motard)){
-                        System.out.println("fichier du patient "+motard+"trouvé" );
-                        System.out.println(str);
-                        break;
-                        
-                        
-                    }else throw new PatientInexistantException("===Ce patient n'existe pas===");
-                    
+        public void modifierpatient(String nomPatient)throws IOException{
+        /*
+        lecture du fichier deja existant
+        creation dans un nouveau fichier
+        recherche de la ligne a modifier
+        si c'est pas la bonne on la reecris dans le nouveau fichier
+        si c'est la bonne : 
+        creation d'un tableau de meme taille 
+        choix entre -copier une case de l'ancien tableau dans le nouveau(au meme numero de case)
+                    -ecrire de nouvelles info a la case du nouveau tableau
+        creation d'un string en parcourant le nouveaut tableau 
+        ecriture de ce string dans le nouveau fichier;
 
+        
+        */
+            
+        File Fichier = new File("ListePatient.txt");
+        File ModifFichier = new File("ListePatientModifie.txt");
+        FileReader FileReader = new FileReader(Fichier);
+        BufferedReader lecture = new BufferedReader(FileReader);
+        FileWriter fileWriter = new FileWriter(ModifFichier);
+        String Ligne = null; 
+
+            while((Ligne = lecture.readLine()) != null){
+                String[] mots = Ligne.split("/");
+               
+                if(mots[1].equalsIgnoreCase(nomPatient)){
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("patient à modifier");
+                    System.out.println(nomPatient); 
+                    String NvMots [] = new String [4];
+                    for(int i=0; i< NvMots.length-1; i++){
+                        System.out.println("Saisir 'e' pour recopier l'information déjà existante \n Saisir 'r' pour ecrire une nouvelle information");
+                        char c = sc.nextLine().charAt(0);
+                        switch(c){
+                            case 'e':
+                            NvMots[i] = mots[i];
+                            break;
+
+                            case'r':
+                            if(i == 0){
+                                System.out.println("saisir l'information du compteur");
+                                String info = sc.nextLine();
+                                NvMots[i] = info;
+                                break;
+                            }else if(i==1){
+                                System.out.println("saisir l'information du nom");
+                                String info = sc.nextLine();
+                                NvMots[i] = info;
+                                break;
+                            }else if(i==2){
+                                System.out.println("saisir l'information du prénom");
+                                String info = sc.nextLine();
+                                NvMots[i] = info;
+                                break;
+                            }else if(i==3){
+                                System.out.println("saisir l'information du numero de securité sociale");
+                               boolean ok = false;
+                               while(!ok){
+                                try{
+                                    String info = sc.nextLine();
+                                    verif(info);
+                                    NvMots[i] = info;
+                                    ok = true;
+                                   }catch(InputMismatchException e){
+                                       System.out.println("erreur");
+                                   }
+                               }
+                                break;
+                            }else if(i==4){
+                                System.out.println("saisir l'information de la date de naissance");
+                                String info = sc.nextLine();
+                                NvMots[i] = info;
+                                break;
+                            }
+                                  
+                        }   
+                    }
+                    // String avec les nouvelles info
+                    // remplisage
+                    //derniere info ne pas mettre de "/" pour eviter les erreur ArrayindexOutOfdBounds au prochain split(); 
+                    String modifLigne = null;
+                    for(int i =0; i< NvMots.length ;i++){
+                         if(NvMots.length -i ==0){
+                            modifLigne += NvMots[i];
+                         } else
+                         modifLigne += NvMots[i]+"/";
+                    }
+                   // ecriture dans le nouveau fichier
+                    fileWriter.write(modifLigne);
+                    fileWriter.write("\n");
+
+                }else{
+                    fileWriter.write(Ligne);
+                    fileWriter.write("\n");
                 }
 
-                lectureFichier.close();
+                 fileWriter.flush();
+                 // fermeture
             }
-        }
+            lecture.close();
+            fileWriter.close();
+            Fichier.delete();
+            // verification que le nouveau fichier a ete renommé 
+            if(ModifFichier.renameTo(Fichier)){
+                System.out.println("Le fichier a été renommé avec succès");
+              }else{
+                System.out.println("Impossible de renommer le fichier");
+              }
+            }
+
+            
+               
+                
+              
+                public void verif(String NbSSocial) throws IOException{
+
+                    //  = numero seciurite sociale 
+                    // verification nombre si c'est des chiffres //si il y en a 13 pile
+               
+                    char Chiffre =' ';
+                    if(NbSSocial.length()<13 || NbSSocial.length()>13){
+                        throw new NbSecuriteSocialeException("pas le bon nombre de caracteres saisis ");
+                    }
+                    // initialisation du tableau de l'alphabet
+                    char Alphabet[] = new char [26];
+                    for(int i=0; i<NbSSocial.length();i++){
+                       Chiffre = NbSSocial.charAt(i);
+                        // utilisation ascii
+                       for(int j =97;j<122;j++){
+                           if(Chiffre ==(Alphabet[i])){
+                               throw new IOException(" erreur de  saisi");
+                               
+                       }
+               
+                    }
+                    
+                    }
+                   }
+            
+        
 
     @Override
    public  String toString() {
