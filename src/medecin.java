2e9f37fef1fdf0c19ae34b762ff5cc4e059c06ee
sir-martino <martin.neron@laposte.net>
2021-12-26 15:44:22 +0100

    new architecture

---
+++ src/medecin.java
@@ -0,0 +1,15 @@
+public class Medecin {
+    public String nom;
+    
+
+    public medecin(String nom) {
+        this.nom = nom;
+    }
+
+    public Consultation rechercherConsultation(int ID){
+
+    }
+    public void ajoutConsultation(){
+       
+    }
+}
