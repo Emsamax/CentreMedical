cfc80b4d3b58474b3ef7e586a97b45c6f143efca
sir-martino <martin.neron@laposte.net>
2021-12-27 15:02:49 +0100

    all methods Medecin

--- src/medecin.java
+++ src/medecin.java
@@ -1,3 +1,5 @@
+import java.security.KeyException;
+
 public class Medecin {
     public String nom;
     
@@ -6,26 +8,34 @@ public class Medecin {
         this.nom = nom;
     }
 
-    public Consultation rechercherConsultation(int ID){
-        //TODO:
-        return new Consultation();
+    public Consultation rechercherConsultation(int ID, BaseConsultation baseCons) throws KeyException{
+        Consultation cons = baseCons.rechercherConsultation(ID);
+        return cons;
     }
-    public void ajoutConsultation(){
-        //TODO:
-    }
-
-    public void supprimerConsultation(int ID){
-        //TODO:
-
-    }
-    public Consultation modifierConsultation(int ID){
-        //TODO:
-        return new Consultation();
-    }
-    public void ajouterPathologie(Patient patient, Pathologie pathologie){
-        //TODO:
-
+    public void ajoutConsultation(BaseConsultation baseCons, Consultation consultation){
+        baseCons.ajouterConsultation(consultation);
     }
 
+    public void supprimerConsultation(BaseConsultation baseCons, int ID){
+        baseCons.supprimerConsultation(ID);
+    }
+
+    public void modifierConsultation(BaseConsultation baseCons, Consultation consultation) throws KeyException{
+        baseCons.modifierConsultation(consultation);
+    }
     
+    public void ajouterPathologie(Patient patient, Pathologie pathologie){
+        // appeler sous la forme Pathologie.COUDE (tout ce qui se trouve dans enum)
+        patient.ajouterPathologie(pathologie);
+
+    }
+    public Consultation createConsultation(String date, Patient patient, String detailsCliniques,AppareilMedical appareilMedical ){  
+        Consultation cons = new Consultation(date, patient, detailsCliniques);
+        cons.octroyerAppareil(appareilMedical);
+        return cons;
+    }
+    public AppareilMedical createAppareilMedical(String nom){
+        AppareilMedical app = new AppareilMedical(nom);
+        return app;
+    }
 }
